dcdb40498b9d342c974ad4c170ff3ea01201a8c9
tranmanhhungdn1201 <tranmanh.hungdn1201@gmail.com>
2021-07-06 21:51:41 +0700

    Fix library js, css

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,54 +7,15 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Employee Roster</title>
 
-        <!-- Bootstrap  -->
-        <!-- <link rel="stylesheet" href="{!! asset('css/bootstrap.min.css') !!}"> -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-        <!-- Bootstrap datetime picker  -->
-        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css"> -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/css/tempusdominus-bootstrap-4.min.css" />
-        <!-- Jquery datatable  -->
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
-        <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css">
-        <!-- Jquery toastr  -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-        <!-- Font-awesome  -->
-        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/fontawesome.min.css" /> -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" />
-        <!-- Custom -->
         <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
         <link rel="stylesheet" href="{!! asset('css/common.css') !!}">
-        <!-- Jquery  -->
-        <script src="{!! asset('js/jquery-3.6.0.min.js') !!}"></script>
-        <!-- Select2  -->
-        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-        <!-- Poper  -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-        <!-- Bootstrap  -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-        <!-- <script src="{!! asset('js/bootstrap.min.js') !!}"></script> -->
-        <!-- Monent  -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment-with-locales.min.js"></script>
-        <!-- Bootstrap datetime picker  -->
-        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/locales/bootstrap-datepicker.vi.min.js"></script> -->
-        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script> -->
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/js/tempusdominus-bootstrap-4.min.js"></script>
-        <!-- Jquery toastr  -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-        <!-- Jquery datatable  -->
-        <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
-        <!-- Custom -->
-        <script src="{!! asset('js/common.js') !!}"></script>
-        <script src="{!! asset('js/javascript.js') !!}"></script>
-
     </head>
 <body>
 @includeWhen(!isset($isNonShowHeader), 'header');
 <main class="site-main container">
+  <script src="{!! asset('js/app.js') !!}"></script>
+  <script src="{!! asset('js/common.js') !!}"></script>
+  <script src="{!! asset('js/javascript.js') !!}"></script>
   @yield('content')
 </main>
 <div class="overlay hidden"></div>
